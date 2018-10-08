914aff03ddd278797d11aded00c3e95fed22e3bc
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 15:47:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    line = "The line is currently: "
+    line = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       line << " #{index + 1}. #{name}"
     end
