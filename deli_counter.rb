0e1bdc939d8948b68e5dccdc23e8baf1130ca242
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 15:40:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,6 @@
 # Write your code here.
 
 def line(katz_deli)
-  i = 0
-  names_plus = ""
-    if katz_deli.length > 0
-      katz_deli.each do |names|
-        i += 1
-        names_plus << "i+1 + .   #{names}"
-          puts "The line is currently: #{names_plus}"
-        end
-      end
 end
 
 
