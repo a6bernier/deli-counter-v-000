b5904993b219ce779abd955b8d30f9adbde3efbe
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 14:20:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(katz_deli)
   names = katz_deli.join(", ")
   if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli}"
+    puts "The line is currently: #{names}"
   else
     puts "The line is currently empty."
   end
