0680e8b31d537920d911751ad004222ab514da89
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 16:05:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,7 @@ def take_a_number(katz_deli, name)
   if katz_deli == []
     katz_deli << name
   else
+    katz_deli << name
     katz_deli.each_with_index do |name, index|
       puts "Welcome, #{name}. You are number #{index + 1} in line."
   end
