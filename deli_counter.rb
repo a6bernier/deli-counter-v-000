5fbf1408aa08f8c5189e142e0bb54615a3b34e64
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 14:19:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(katz_deli)
-  if katz_deli.length < 0
+  if katz_deli.length > 0
     puts "The line is currently: #{katz_deli}"
   else
     puts "The line is currently empty."
