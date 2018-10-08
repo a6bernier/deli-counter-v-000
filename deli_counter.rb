a6af3b37b88da087baa9d2c03f76fc0c109bdcec
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 15:42:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 
 def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |name, index|
+      puts "#{index} + . #{name} "
 end
 
 
