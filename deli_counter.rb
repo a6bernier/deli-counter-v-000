738a862a7e8173f078807d3e0307c5383eca220b
Andrew Bernier <a6bernier@gmail.com>
2018-10-08 14:28:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(katz_deli)
     i += 1
     names_plus << i + ". " names
       puts "The line is currently: #{names_plus}"
+    end
   else
     puts "The line is currently empty."
-  end
   end
 
 
